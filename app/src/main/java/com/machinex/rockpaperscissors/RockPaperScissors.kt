6da816d5401b15a5d15bc72cc21c67099ae79a7c
Robert Angote <ebobby@outlook.com>
2024-01-16 19:58:26 +0300

    Discerning the winner.

--- app/src/main/java/com/machinex/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/machinex/rockpaperscissors/RockPaperScissors.kt
@@ -2,31 +2,40 @@ package com.machinex.rockpaperscissors
 
 fun main()
 {
-    var computerChoice = "";
-    var playerChoice = "";
+    var computerChoice = ""
+    var playerChoice = ""
 
     //Prompt user to make a choice among the 3 option, rock, paper or scissors
     println("Rock, Paper or Scissors? What's your choice, pick one...");
 
     //Capture the player's choice
-    playerChoice = readln();
+    playerChoice = readln()
 
     //Generate a random number between 1 & 3
-    val randomNumber = (1..3).random();
+    val randomNumber = (1..3).random()
 
     //Comparing random numbers against the RPS
-    if(randomNumber == 1)
-    {
-        computerChoice = "Rock";
-    }
-    else if(randomNumber == 2)
-    {
-        computerChoice = "Paper";
-    }
-    else
-    {
-        computerChoice = "Scissors";
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        else -> {
+            computerChoice = "Scissors"
+        }
     }
 
-    println(computerChoice);
+    //println(computerChoice)
+
+    //Comparing user and Comp selections
+    val winner = when
+    {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else->"Computer"
+    }
 }

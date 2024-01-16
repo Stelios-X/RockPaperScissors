59d755b8612aff34f896ff75e10072c4ef099a56
Robert Angote <ebobby@outlook.com>
2024-01-16 19:25:21 +0300

    Random number generator with defined scope and comparison between input and Randomizer.

--- app/src/main/java/com/machinex/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/machinex/rockpaperscissors/RockPaperScissors.kt
@@ -4,4 +4,29 @@ fun main()
 {
     var computerChoice = "";
     var playerChoice = "";
+
+    //Prompt user to make a choice among the 3 option, rock, paper or scissors
+    println("Rock, Paper or Scissors? What's your choice, pick one...");
+
+    //Capture the player's choice
+    playerChoice = readln();
+
+    //Generate a random number between 1 & 3
+    val randomNumber = (1..3).random();
+
+    //Comparing random numbers against the RPS
+    if(randomNumber == 1)
+    {
+        computerChoice = "Rock";
+    }
+    else if(randomNumber == 2)
+    {
+        computerChoice = "Paper";
+    }
+    else
+    {
+        computerChoice = "Scissors";
+    }
+
+    println(computerChoice);
 }
